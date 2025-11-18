257bd39ba0d28c2371952fba86a0cda249f9a2a3
dohee12 <happyddo42@gmail.com>
2025-11-18 20:41:43 +0900

    11월 2일 ì 코드 오류 수정

--- src/main/java/com/example/portfoliospring1/infra/feign/KauthFeignClient.java
+++ src/main/java/com/example/portfoliospring1/infra/feign/KauthFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
-@FeignClient(name="kauth", url="http://kauth.kakao.com")
+@FeignClient(name="kauth", url="https://kauth.kakao.com")
 public interface KauthFeignClient {
     @PostMapping("/oauth/token")
     KauthTokenDto getKakaoToken(
